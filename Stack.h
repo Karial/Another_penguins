5a8d86cf843d13eb8b8f7948f401c7ae2f0ddb22
Karial <48125405+Karial@users.noreply.github.com>
2019-10-01 21:44:33 +0300

    Add files via upload

--- Stack.h
+++ Stack.h
@@ -28,19 +28,28 @@ public:
 	StackIterator begin() const override;
 };
 
-class StackIterator {
+class AbstractIterator {
+	virtual std::string Val() const = 0;
+	virtual bool Next() = 0;
+	virtual bool Prev() = 0;
+	virtual bool HasNext() const = 0;
+	virtual bool HasPrev() const = 0;
+	virtual bool IsValid() const = 0;
+};
+
+class StackIterator: public AbstractIterator {
 private:
 	const Stack* s;
 	int ind;
 public:
 	StackIterator() = default;
 	StackIterator(const Stack& s_, const int ind_ = 0);
-	std::string Val() const;
-	bool Next();
-	bool Prev();
-	bool HasNext() const;
-	bool HasPrev() const;
-	bool IsValid() const;
+	std::string Val() const override;
+	bool Next() override;
+	bool Prev() override;
+	bool HasNext() const override;
+	bool HasPrev() const override;
+	bool IsValid() const override;
 	void Copy(const StackIterator& copy);
 };
 
